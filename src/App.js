2bda186b9b7441823bc2860e2ef1c22b263569b7
Lazlo Benny <stewbradley@gmail.com>
2023-02-07 12:08:10 -0700

    about to go over props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-
+import Employee from "./components/Employee";
 
 function App() {
+  const showEmployees = true;
   return (
     <div className="App">
-  
-      <header className="App-header">
       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {  showEmployees ? (
+          <>
+            <Employee />
+            <Employee />
+            <Employee />
+            <Employee />
+          </> 
+      ) : (
+        <p>You do not have access to the employee list</p>
+      )}
+      
     </div>
   );
 }
