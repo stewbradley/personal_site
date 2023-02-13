588c2c4856d5f1a9c4abec5982501b43396bd134
Lazlo Benny <stewbradley@gmail.com>
2023-02-13 11:57:06 -0700

    style tweaks

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
+  
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -40,7 +42,9 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                   id="name" 
                   type="text" 
-                  value="Jane Doe" />
+                  value={name}
+                  onChange={(e) => {setName(e.target.value)}}
+                />
               </div>
             </div>
             <div className="md:flex md:items-center mb-6">
@@ -57,7 +61,8 @@ function EditEmployee() {
                   className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                   id="role" 
                   type="text" 
-                  value="Jane Doe" />
+                  value={role}
+                  onChange={(e) => {setRole(e.target.value)}} />
               </div>
             </div>
            
@@ -70,7 +75,12 @@ function EditEmployee() {
           >
             Close
           </button>
-          <button className="bg-blue-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" form="editModal">Update</button>
+          <button 
+            className="bg-blue-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded" 
+            form="editModal"
+          >
+            Update
+          </button>
         </Modal.Footer>
       </Modal>
     </>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,7 +3,7 @@ import EditEmployee from "./EditEmployee";
 function Employee(props) {
     return (
         // don't change props here, change them in App.js
-    <div className="m-2 py-8 px-8 max-w-sm bg-slate-100 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="min-w[350px] max-w-[350px] m-2 py-8 px-8 bg-slate-100 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img 
             className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
             src={props.img} 
@@ -18,7 +18,10 @@ function Employee(props) {
             </p>
         </div>
         
-        <EditEmployee />
+        <EditEmployee 
+            name={props.name} 
+            role={props.role}
+        />
 
         </div>
     </div>
