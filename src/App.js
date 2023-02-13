f54ba4660ace8c7baedf17d3432d0962af0768bd
Lazlo Benny <stewbradley@gmail.com>
2023-02-13 11:38:47 -0700

    adding a modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
+import './index.css';
 import Employee from "./components/Employee";
-
+import { useState } from "react"; 
+import { v4 as uuidv4 } from 'uuid';
+// useState is an example of Hook. When things start with use, they are hooks
+// hooks allow us to use state in functional components, without writing a class
 function App() {
+  const [role, setRole] = useState('dev');
+  // number 1 rule for state, never assign it directly
+  const [employees, setEmployees] = useState([
+       {
+        name: "Stew", 
+        role: "Admin", 
+        img: "https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg",
+      },
+      {
+        name: "Kunal", 
+        role: "Admin", 
+        img: 'https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg',
+      },
+      {
+        name: "Behzod", 
+        role: "Contributor", 
+        img: "https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg",
+      },
+      {
+        name: "Nick", 
+        role: "Contributor", 
+        img: "https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg",
+      }
+    ]);
   const showEmployees = true;
   return (
-    <div className="App">
-      
-      {  showEmployees ? (
+    <div className= "App">
+        {showEmployees ? (
           <>
-            <Employee />
-            <Employee />
-            <Employee />
-            <Employee />
+            <input 
+              type="text" 
+              onChange={(e) => {
+                console.log(e.target.value);
+                setRole(e.target.value)
+              }}
+            />
+            <div className="flex flex-wrap">
+              {employees.map((employee) => {
+                return (
+                  <Employee 
+                    key={uuidv4()}
+                    name={employee.name} 
+                    role={employee.role} 
+                    img={employee.img} 
+                  />
+                );
+              })}
+
+            </div>
           </> 
       ) : (
         <p>You do not have access to the employee list</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,28 @@
-function Employee() {
-    return <h3>Here is an employee!</h3>;
+import EditEmployee from "./EditEmployee";
+
+function Employee(props) {
+    return (
+        // don't change props here, change them in App.js
+    <div className="m-2 py-8 px-8 max-w-sm bg-slate-100 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img 
+            className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
+            src={props.img} 
+            alt="Face"/>
+        <div className="text-center space-y-2 sm:text-left">
+        <div className="space-y-0.5">
+            <p className="text-lg text-whi font-semibold">
+                {props.name}
+            </p>
+            <p className="text-slate-500 font-medium">
+                {props.role}
+            </p>
+        </div>
+        
+        <EditEmployee />
+
+        </div>
+    </div>
+    )
  }
 
 export default Employee;
