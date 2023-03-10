9ba8119377627befe9da80aad3cfecb4f8001c8b
Lazlo Benny <stewbradley@gmail.com>
2023-03-10 12:03:38 -0700

    tweaking header spacing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './index.css';
 import Employees from './pages/Employees';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contributors from './pages/Contributors';
 // useState is an example of Hook. When things start with use, they are hooks
 // hooks allow us to use state in functional components, without writing a class
 function App() {
   // number 1 rule for state, never assign it directly
-  return <Employees />;
+  // header must be in the BrowserRouter, otherwise it will not work
+  return (
+    <BrowserRouter>
+        <Header> 
+            <Routes>
+                <Route path='/employees' element={<Employees />} />
+                <Route path='/contributors' element={<Contributors />} /> 
+            </Routes>
+        </Header> 
+    </BrowserRouter>
+      
+  );
 }
 
 export default App;
